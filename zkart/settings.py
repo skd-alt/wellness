d1fecd13100504654dea48c131f875f87abd1293
skd-alt <sipho@skanalytics.co.za>
2022-06-17 12:36:02 +0200

    Update settings.py

--- zkart/settings.py
+++ zkart/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 from decouple import config
 import django_heroku
@@ -136,10 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
-STATICFILES_DIRS = [
-    'zkart/static',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'zkart/static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'

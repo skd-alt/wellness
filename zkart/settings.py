52e9ea2cf68f1e4914921dc389df3f4dd8b9d479
skd-alt <sipho@skanalytics.co.za>
2022-06-17 14:02:15 +0200

    Update settings.py

--- zkart/settings.py
+++ zkart/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'zkart/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
